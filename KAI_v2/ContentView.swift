d517dccdf14f310280bc0b191f64d74b243f7d0c
VildanovRR <ravil.vildanov@akbarsdigital.ru>
2020-01-07 18:46:39 +0300

    Initial Commit

---
+++ KAI_v2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KAI_v2
+//
+//  Created by Ravil Vildanov on 07.01.2020.
+//  Copyright © 2020 Ravil Vildanov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
